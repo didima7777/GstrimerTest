d14711e1dc10417f1415bc2ec3ebcd3ceae7291a
dima <didima7777@gmail.com>
2015-02-27 20:42:47 +0300

    add new buffer

--- test_gst.cpp
+++ test_gst.cpp
@@ -2,31 +2,36 @@
 #include <string.h>
 #include <stdlib.h>
 #include <gst/gst.h>
+#include <gst/app/gstappsink.h>
 //#include <gst/app/gstappsrc.h>
 //#include <gst/app/gstappbuffer.h>
 #include <gst/app/gstappsink.h>  
 #include <opencv2/opencv.hpp>
 
-void print_buffer (GstBuffer *buffer, const char *title);
+GMainLoop *loop;
 
-static GstFlowReturn new_buffer_list (GstAppSink *sink, gpointer user_data)
+void print_buffer(GstAppSink *sink, const char *title);
+
+static void new_oes (GstAppSink *sink, gpointer user_data)
 { 
+      printf("###### eos #######\n");	
 //    GstBufferList *list = gst_app_sink_pull_buffer_list (sink);
 //    GstBufferListIterator *it = gst_buffer_list_iterate (list);
-    GstBuffer *buffer;
+//    GstBuffer *buffer;
 //    while (gst_buffer_list_iterator_next_group (it))
 //        while ((buffer = gst_buffer_list_iterator_next (it)) != NULL) print_buffer(buffer, "new_buffer_list");
 //    gst_buffer_list_iterator_free (it);
  
-    return GST_FLOW_OK;
+ 
 }
  
 static GstFlowReturn new_preroll (GstAppSink *sink, gpointer user_data)
 {
-//    GstBuffer *buffer =  gst_app_sink_pull_preroll (sink);
-//    if (buffer) print_buffer(buffer, "preroll");
+      printf("#####  new_preroll #######!!!");	
+     GstSample *buffer =  gst_app_sink_pull_preroll (sink);
+     if (buffer) print_buffer(sink, "preroll");
 
-
+	gst_sample_unref (buffer);
     return GST_FLOW_OK;
 }
 
@@ -37,8 +42,19 @@ static GstFlowReturn new_buffer(GstAppSink *sink, gpointer user_data)
 {
    int height=480;
    int width=640;
+   GstMapInfo map;
+
+   frame = cvCreateImage(cvSize(width,height), IPL_DEPTH_8U,2);	
+   m_RGB = cvCreateImage(cvSize(width,height), IPL_DEPTH_8U, 3);
+
+   printf("####	app buffer   ####\n");	
+   GstSample *sample =  gst_app_sink_pull_sample(sink);
+   GstBuffer *buffer =  gst_sample_get_buffer (sample);
+   gst_buffer_map (buffer, &map, GST_MAP_READ);
+   char *pData=new char[480*640*3];
+   if (buffer) print_buffer(sink, "buffer"); else printf("NULL new buffer");
  
-/*
+
 	for(int i = 0, j=0; i < width*height * 3; i+=6, j+=2)  //+2
 	{
 	    m_RGB->imageData[i] = pData[j+0] ;
@@ -49,15 +65,30 @@ static GstFlowReturn new_buffer(GstAppSink *sink, gpointer user_data)
 	    m_RGB->imageData[i+5] = pData[j+1];
 	}
 
-	cv::Mat mat_img(m_RGB);
-	cv::imwrite( "my_bitmap.bmp", mat_img);		
-*/
-
+	  cv::Mat mat_img(m_RGB);
+	//cv::imwrite( "my_bitmap.bmp", mat_img);		
+      gst_buffer_unmap (buffer, &map);
+      gst_sample_unref (sample);
+      gst_buffer_unref (buffer);
+     gst_app_sink_is_eos(sink);
     return GST_FLOW_OK;
 }
 
-void print_buffer (GstBuffer *buffer, const char *title)
+void print_buffer (GstAppSink *sink, const char *title)
 {
+    GstCaps *caps = gst_app_sink_get_caps(sink);
+    for (uint j = 0; j < gst_caps_get_size(caps); ++j) {
+        GstStructure *structure = gst_caps_get_structure(caps, j);
+        printf("%s{%ss}: ",(title),(gst_structure_get_name(structure)));
+        for (int i = 0; i < gst_structure_n_fields(structure); ++i) {
+            if (i != 0)
+                printf(", ");
+            const char *name = gst_structure_nth_field_name(structure, i);
+            GType type = gst_structure_get_field_type(structure, name);
+            printf("%s[%s]",(name),(g_type_name(type)));
+        }
+        printf("\n");
+    }
 }
 
 
@@ -79,17 +110,20 @@ static gboolean bus_call (GstBus *bus, GstMessage *msg, gpointer user_data)
         GError *err;
  
         gst_message_parse_error (msg, &err, &debug);
-	printf("!!! [%s]: %s %s\n", GST_OBJECT_NAME (msg->src), err->message, debug);
+	printf("[%s]: %s %s\n", GST_OBJECT_NAME (msg->src), err->message, debug);
         g_free (debug);
         g_error_free (err);
  	exit(1);
         break;
     }
-    default: {
-/*
-        const GstStructure *structure = msg->structure;
+   case GST_MESSAGE_EOS:
+	/* end-of-stream */
+	g_main_loop_quit (loop);
+   break;
+   default: {
+	const GstStructure *structure = gst_message_get_structure(msg);
         if (structure) {
-                printf("%s{%s}",gst_message_type_get_name (msg->type),gst_structure_get_name(structure));
+                printf("%s{%s} ",gst_message_type_get_name (msg->type),gst_structure_get_name(structure));
             for (i = 0; i < gst_structure_n_fields(structure); ++i) {
                 if (i != 0) printf(", ");
                 const char *name = gst_structure_nth_field_name(structure, i);
@@ -101,7 +135,7 @@ static gboolean bus_call (GstBus *bus, GstMessage *msg, gpointer user_data)
         } else {
             printf("%s{}\n", gst_message_type_get_name (msg->type));
         }
-*/
+
         break;
     }
     }
@@ -123,7 +157,7 @@ int main(int argc, char *argv[]) {
   enc	= gst_element_factory_make ("jpegenc", "jpegenc");
   sink_app = gst_element_factory_make ("appsink", "appsink");
   sink_file = gst_element_factory_make ("filesink", "filesink");
-  g_object_set( G_OBJECT(queue), "max-size-buffers", 3, NULL);
+  g_object_set( G_OBJECT(queue), "max-size-buffers", 10, NULL);
 
   g_object_set( G_OBJECT(source), "device", "/dev/video0", NULL);
 //  g_object_set( G_OBJECT(source), "num-buffers", "-1", NULL);
@@ -169,11 +203,21 @@ int main(int argc, char *argv[]) {
          exit (1);
     }
 
-  bus = gst_element_get_bus (pipeline);
+  bus = gst_element_get_bus (pipeline); 
+
+
+  GstAppSinkCallbacks callbacks;
+  callbacks.eos=&new_oes;
+  callbacks.new_preroll=&new_preroll;
+  callbacks.new_sample=&new_buffer;
+
+  gst_app_sink_set_callbacks (GST_APP_SINK(sink_app), &callbacks, NULL, NULL);
+  guint  bus_watch_id = gst_bus_add_watch (bus, bus_call, NULL);
+
   ret = gst_element_set_state (pipeline, GST_STATE_PLAYING);
 
 
-  GMainLoop *loop = g_main_loop_new (NULL, FALSE);    
+  loop = g_main_loop_new (NULL, FALSE);    
   g_main_loop_run(loop);
 
 
