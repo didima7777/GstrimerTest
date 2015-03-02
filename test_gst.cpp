811bc15c965c899abda8729115fe990412ff0a13
didima7777 <didima7777@gmail.com>
2015-03-03 01:50:11 +0300

    change code

--- test_gst.cpp
+++ test_gst.cpp
@@ -33,14 +33,6 @@ static void convert(unsigned char *src, char *dst, int size) {
 
 static void new_oes(GstAppSink *sink, gpointer user_data) {
     printf("###### eos #######\n");
-    //    GstBufferList *list = gst_app_sink_pull_buffer_list (sink);
-    //    GstBufferListIterator *it = gst_buffer_list_iterate (list);
-    //    GstBuffer *buffer;
-    //    while (gst_buffer_list_iterator_next_group (it))
-    //        while ((buffer = gst_buffer_list_iterator_next (it)) != NULL) print_buffer(buffer, "new_buffer_list");
-    //    gst_buffer_list_iterator_free (it);
-
-
 }
 
 static GstFlowReturn new_preroll(GstAppSink *sink, gpointer user_data) {
@@ -58,29 +50,34 @@ static GstFlowReturn new_preroll(GstAppSink *sink, gpointer user_data) {
 
 IplImage *frame = NULL;
 IplImage *m_RGB = NULL;
+unsigned char *buf_tmp = NULL;
+GMutex mutex;
 
 static GstFlowReturn new_buffer(GstAppSink *sink, gpointer user_data) {
     int height = 480;//1944;
     int width = 640;//2592;
+    static int  cnt=0;
+
     GstMapInfo map;
 
     GstSample* sample;
     GstBuffer* buffer;
     GstCaps* caps;
-    printf("#");
+    printf("# %d #\n",cnt++);
+    
+    g_mutex_lock(&mutex);
     sample = gst_app_sink_pull_sample(sink);
-    //    frame = cvCreateImage(cvSize(width,height), IPL_DEPTH_8U,2);	
-    //m_RGB = cvCreateImage(cvSize(width,height), IPL_DEPTH_8U, 3);
-
     if (sample) {
-        buffer = gst_sample_get_buffer(sample);
-        caps = gst_sample_get_caps(sample);
-        gst_buffer_map(buffer, &map, GST_MAP_READ);
-        printf("size = %d  ", map.size);
-        unsigned char *pData = (unsigned char*) map.data;
-        if (m_RGB==NULL) m_RGB = cvCreateImage(cvSize(width, height), IPL_DEPTH_8U, 3);
-        convert(pData, m_RGB->imageData, height * width);
 
+            buffer = gst_sample_get_buffer(sample);
+            caps = gst_sample_get_caps(sample);
+            gst_buffer_map(buffer, &map, GST_MAP_READ);
+            printf("size = %d  ", map.size);
+            unsigned char *pData = (unsigned char*) map.data;
+            if (m_RGB==NULL) m_RGB = cvCreateImage(cvSize(width, height), IPL_DEPTH_8U, 3);
+            if (buf_tmp ==NULL) buf_tmp=new unsigned char[width*height*3];
+            memcpy((void*)buf_tmp,(void*)pData,width*height*2);
+            convert(buf_tmp, m_RGB->imageData, height * width);
 //        cv::Mat mat_img(m_RGB);
 //        cv::imwrite("my_bitmap.bmp", mat_img);
 //        mat_img.release();
@@ -90,11 +87,13 @@ static GstFlowReturn new_buffer(GstAppSink *sink, gpointer user_data) {
         gst_buffer_unref(buffer);
         printf("\n");
     }
+    g_mutex_unlock(&mutex);
     return GST_FLOW_OK;
 }
 
 void print_buffer(GstAppSink *sink, const char *title) {
-    GstCaps *caps = gst_app_sink_get_caps(sink);
+    g_mutex_lock(&mutex);
+    GstCaps *caps = gst_app_sink_get_caps(sink);    
     for (uint j = 0; j < gst_caps_get_size(caps); ++j) {
         GstStructure *structure = gst_caps_get_structure(caps, j);
         printf("%s{%ss}: ", (title), (gst_structure_get_name(structure)));
@@ -107,6 +106,7 @@ void print_buffer(GstAppSink *sink, const char *title) {
         }
         printf("\n");
     }
+    g_mutex_unlock(&mutex);
 }
 
 static gboolean bus_call(GstBus *bus, GstMessage *msg, gpointer user_data) {
@@ -257,8 +257,6 @@ int main(int argc, char *argv[]) {
     pipeline_v2 = gst_pipeline_new("cam-pipeline");
 
 
-
-
     if (!pipeline_v1 || !pipeline_v2 || !source || !sink_app || !sink_file || !queue1 || !queue2 || !videotee || !enc || !parser) {
         g_printerr("Not pipeline element could be created.\n");
         return -1;
@@ -360,12 +358,12 @@ int main(int argc, char *argv[]) {
     gst_app_sink_set_callbacks(GST_APP_SINK(sink_app), &callbacks, NULL, NULL);
     //g_object_set (sink_app, "emit-signals", TRUE, NULL);
     //g_signal_connect (sink_app, "new-sample", G_CALLBACK (new_buffer), NULL);
-    g_signal_connect (udpsink, "client-added", G_CALLBACK (&add_cliden), NULL);
+    //g_signal_connect (udpsink, "client-added", G_CALLBACK (&add_cliden), NULL);
     
     //guint bus_watch_id = gst_bus_add_watch(bus, bus_call, NULL);  
     
-    gst_bus_add_signal_watch (bus);
-    g_signal_connect (bus, "message", G_CALLBACK (bus_call), &data);
+    //gst_bus_add_signal_watch (bus);
+    //g_signal_connect (bus, "message", G_CALLBACK (bus_call), &data);
     
     ret = gst_element_set_state(pipeline_v1, GST_STATE_PLAYING);
 
